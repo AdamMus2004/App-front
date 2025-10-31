d24fb0d22c24d9fc72998b35ef7957f14323df44
Adam Mus <adammuspraca@gmail.com>
2025-10-31 23:50:17 +0100

    Day 2: Update RegisterPage

--- Desktop/app-frontend/frontend/src/api/axios.tsx
+++ Desktop/app-frontend/frontend/src/api/axios.tsx
@@ -3,16 +3,19 @@ import axios from 'axios';
 // UserService
 export const userApi = axios.create({
     baseURL: 'http://localhost:8080',
+    withCredentials: true,
 });
 
 // ProfileService
 export const profileApi = axios.create({
     baseURL: 'http://localhost:8081',
+    withCredentials: true,
 });
 
 // WilksService
 export const wilksApi = axios.create({
     baseURL: 'http://localhost:8082',
+    withCredentials: true,
 });
 
 export default userApi;
